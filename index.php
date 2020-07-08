99b7cc85cc79bbbd85af84bfa9cee72d7c91c850
RMMacuacua <40232102+RMMacuacua@users.noreply.github.com>
2020-07-08 10:44:20 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+ <title>Send HTTP Get Request using PHP</title>
+ <script src="https://code.jquery.com/jquery-latest.js"></script>
+	<script>
+	  function submit_soap(){
+		var ip=$("#ip_address").val();
+		$.get("form_get.php",{ip:ip},
+		function(data){
+		  $("#json_response").html(data);
+		});
+	}
+	</script>
+</head>
+<body>
+  <center>
+    <h3>Send HTTP Get Request using PHP</h3>
+     <form>
+     IP Address : <input name="ip_address" id="ip_address" type="text" /><br />
+      <input type="button" value="Submit" onclick="submit_soap()"/>
+    </form>
+       <br>-----------
+	  <div id="json_response"></div>
+   </center>
+</body>
+</html>
